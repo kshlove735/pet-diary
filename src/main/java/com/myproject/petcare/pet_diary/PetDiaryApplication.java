7819e1935875002e749e5723ee8491890098e078
kshlove735 <kshlove735@gmail.com>
2025-02-14 18:03:32 +0900

    초기 설정

---
+++ src/main/java/com/myproject/petcare/pet_diary/PetDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.petcare.pet_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetDiaryApplication.class, args);
+	}
+
+}
