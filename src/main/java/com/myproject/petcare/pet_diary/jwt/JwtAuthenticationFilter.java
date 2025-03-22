7ff6b9fe2c3297c09e9199f26b1b2052f8fc1b45
kshlove735 <kshlove735@gmail.com>
2025-03-22 16:17:07 +0900

    JwtAuthenticationFilter에 인가가 필요없는 경로 설정 할 경우

--- src/main/java/com/myproject/petcare/pet_diary/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/myproject/petcare/pet_diary/jwt/JwtAuthenticationFilter.java
@@ -1,36 +1,56 @@
 package com.myproject.petcare.pet_diary.jwt;
 
+import com.myproject.petcare.pet_diary.common.exception.custom_exception.TokenNotFoundException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
-@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
     private final CustomUserDetailsService customUserDetailsService;
+
+    // 인가가 필요없는 경로 설정 할 경우
+    private final List<String> excludedPatterns = Arrays.asList("/", "/v1/auth/**");
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, CustomUserDetailsService customUserDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    // 인가가 필요없는 경로 설정 할 경우
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String requestURI = request.getRequestURI();
+        return excludedPatterns.stream().anyMatch(pattern -> pathMatcher.match(pattern, requestURI));
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         // request에서 Authorization 헤더 찾음
         String authorization = request.getHeader("Authorization");
-
+        log.info("Auth = {}", authorization);
         // Authorization 헤더 검증
         if (authorization == null || !authorization.startsWith("Bearer ")) {
-            log.info("토큰이 없습니다.");
-            filterChain.doFilter(request, response);
-            return;
+            // 인가가 필요없는 경로 설정 할 경우
+            throw new TokenNotFoundException("토큰이 없습니다.");
+
+            //filterChain.doFilter(request, response);
+            //return;
         }
 
         // Bearer 부분 제거 후 순수 토근만 획득
@@ -53,7 +73,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // 스프링 시큐리티 인증 토큰 생성
         Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
 
-        // 센션 사용자 등록
+        // 임시 센션에 사용자 등록
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
         filterChain.doFilter(request, response);
